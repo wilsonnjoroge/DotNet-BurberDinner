8de6ab8fccf1fa8608ba9d70619040bac4bf2727
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-05-26 19:54:15 +0300

    added a controller folder and defines the authentication controller that has a log in and register methods

---
+++ BurberDinner.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,68 @@
+
+using BurberDinner.Application.Services.Authentication;
+using BurberDinner.Contracts.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BurberDinner.Api.Controllers
+{
+  [ApiController]
+  [Route("auth")]
+  public class AuthenticationController : ControllerBase
+  {
+
+    private readonly  IAuthenticationService _authenticationService;
+    public AuthenticationController(IAuthenticationService authenticationService)
+    {
+      _authenticationService = authenticationService;
+    }
+
+    [HttpPost("register")]
+    public IActionResult Register(RegisterRequest request)
+    {
+      // Use the injected IAuthenticationService to obtain register method
+      var authResult = _authenticationService.Register(
+        request.FirstName, 
+        request.LastName, 
+        request.Email, 
+        request.Password
+        );
+
+      // Map the response to the authentication result defined
+        var response = new AuthenticationResponse
+        (
+          authResult.Id,
+          authResult.FirstName,
+          authResult.LastName,
+          authResult.Email,
+          authResult.Token
+        );
+      // Return the response
+      return Ok(response);
+    }
+
+
+    [HttpPost("login")]
+    public IActionResult Login(LoginRequest request)
+    {
+      // Use the injected IAuthenticationService to obtain login method
+      var authResult = _authenticationService.Login(
+        request.Email, 
+        request.Password
+        );
+
+      // Map the response to the authentication result defined
+        var response = new AuthenticationResponse
+        (
+          authResult.Id,
+          authResult.FirstName,
+          authResult.LastName,
+          authResult.Email,
+          authResult.Token
+        );
+      // Return the response
+      return Ok(response);
+    }
+
+  }
+}
+
