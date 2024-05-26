b0d60552cc85adb6b7376b6477c17543a5b954dd
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-05-26 19:55:03 +0300

    injected the authentication servise interface and class implementing it as add scopped

--- BurberDinner.Api/Program.cs
+++ BurberDinner.Api/Program.cs
@@ -1,6 +1,9 @@
+using BurberDinner.Application.Services.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
 
 {
+    builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
     builder.Services.AddControllers();
 }
 
