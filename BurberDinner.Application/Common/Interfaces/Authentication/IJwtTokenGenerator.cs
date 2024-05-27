55d94fe2585a9f3dcd4c4604d14aef9d27c6c140
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-05-27 15:57:23 +0300

    added authentication folder in the intefaces

---
+++ BurberDinner.Application/Common/Interfaces/Authentication/IJwtTokenGenerator.cs
@@ -0,0 +1,8 @@
+
+namespace BurberDinner.Application.Common.Interfaces.Authentication
+{
+  public interface IJwtTokenGenerator
+  {
+    string GenerateToken(Guid userId, string firstName, string lastName);
+  }
+}
