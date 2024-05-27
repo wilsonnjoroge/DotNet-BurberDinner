050213e05b5f2c371230389e62d4a68f3e1b8aa9
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-05-27 16:02:10 +0300

    added authentication folder to house the jwt setting class and clss to generate token

---
+++ BurberDinner.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -0,0 +1,54 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using BurberDinner.Application.Common.Interfaces.Authentication;
+using BurberDinner.Appliction.Common.Interfaces.Services;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BurberDinner.Infrastructure.Authentication
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        private readonly IDateTimeProvider _dateTimeProvider;
+        private readonly JwtSettings _jwtSettings;
+
+        public JwtTokenGenerator(IDateTimeProvider dateTimeProvider, IOptions<JwtSettings> jwtOptions)
+        {
+            _dateTimeProvider = dateTimeProvider;
+            _jwtSettings = jwtOptions.Value;
+
+            // Log the settings to verify they are loaded correctly
+            Console.WriteLine($"JWT Secret: {_jwtSettings.Secret}");
+            Console.WriteLine($"JWT Issuer: {_jwtSettings.Issuer}");
+            Console.WriteLine($"JWT Audience: {_jwtSettings.Audience}");
+            Console.WriteLine($"JWT ExpiryMinutes: {_jwtSettings.ExpiryMinutes}");
+        }
+
+        public string GenerateToken(Guid userId, string firstName, string lastName)
+        {
+            var signingCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Secret)),
+                SecurityAlgorithms.HmacSha256
+            );
+
+            var claims = new []
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
+                new Claim(JwtRegisteredClaimNames.GivenName, firstName),
+                new Claim(JwtRegisteredClaimNames.FamilyName, lastName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+            var securityToken = new JwtSecurityToken(
+                issuer: _jwtSettings.Issuer,
+                audience: _jwtSettings.Audience,
+                expires: _dateTimeProvider.UtcNow.AddMinutes(_jwtSettings.ExpiryMinutes),
+                claims: claims,
+                signingCredentials: signingCredentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(securityToken);
+        }
+    }
+}
