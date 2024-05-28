e706bb5d61a9aeebd5264a0106dbc66565e1245c
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-05-28 13:06:43 +0300

    created a persistence folder to house the user repository class

---
+++ BurberDinner.Infrastructure/Persistence/UserRepository.cs
@@ -0,0 +1,21 @@
+
+using BurberDinner.Application.Common.Interfaces.Persistence;
+using BurberDinner.Domain.Entities;
+
+namespace BurberDinner.Infrastructure.Persistence
+{
+    public class UserRepository : IUserRepository
+    {
+        private static readonly List<User> _users = new ();
+
+        public void AddUser(User user)
+        {
+            _users.Add(user);
+        }
+
+        public User? GetUserByEmail(string email)
+        {
+          return _users.SingleOrDefault(u => u.Email == email);
+        }
+    }
+}
