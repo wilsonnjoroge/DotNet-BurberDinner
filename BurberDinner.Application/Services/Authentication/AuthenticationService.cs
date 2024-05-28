9c2b0dd00c1d67435ba90bd3f786f493c0520f92
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-05-28 13:08:44 +0300

    restructured the code to take the fields from the user class instead of parsing individually

--- BurberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BurberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,32 +1,45 @@
 
 using BurberDinner.Application.Common.Interfaces.Authentication;
+using BurberDinner.Application.Common.Interfaces.Persistence;
+using BurberDinner.Domain.Entities;
 
 namespace BurberDinner.Application.Services.Authentication
 {
     public class AuthenticationService : IAuthenticationService
     {
         private readonly IJwtTokenGenerator _jwtTokenGenerator;
+        private readonly IUserRepository _userRepository;
 
-        public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+        public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository)
         {
             _jwtTokenGenerator = jwtTokenGenerator;
+            _userRepository = userRepository;
         }
+
         public AuthenticationResult Register(string firstName, string lastName, string email, string password)
         {
-            // Check if user exists in the db
+            // Validate user !exists in the db
+            if(_userRepository.GetUserByEmail(email) is not null)
+            {
+                throw new Exception("User with provided email already exists");
+            }
 
-            // Create user(Generate unique id)
+            // Create user(Generate unique id) and persist in to DB
+            var user = new User{
+                FirstName = firstName, 
+                LastName = lastName, 
+                Email = email, 
+                Password = password
+            };
+
+            _userRepository.AddUser(user);            
 
             //Generate Token
-            Guid userId = Guid.NewGuid();
-            var token = _jwtTokenGenerator.GenerateToken( userId, firstName, lastName);
+            var token = _jwtTokenGenerator.GenerateToken( user);
 
 
             return new AuthenticationResult(
-              Guid.NewGuid(), 
-              firstName, 
-              lastName, 
-              email, 
+              user, 
               token
             );
         }
@@ -34,12 +47,24 @@ namespace BurberDinner.Application.Services.Authentication
         
         public AuthenticationResult Login(string email, string password)
         {
-           return new AuthenticationResult(
-              Guid.NewGuid(), 
-              "firstName", 
-              "lastName", 
-              email, 
-              "token"
+            // Validate the user exists
+            var user = _userRepository.GetUserByEmail(email);
+            if(user is null)
+            {
+                throw new Exception("User with provided email does not exists");
+            }
+
+            // Validate password is correct
+            if(user.Password!=  password)
+            {
+                throw new Exception("Invalid password");
+            }
+
+            // Create jwt token
+            var token = _jwtTokenGenerator.GenerateToken( user);
+            return new AuthenticationResult(
+              user,
+              token
               );
         }
 
