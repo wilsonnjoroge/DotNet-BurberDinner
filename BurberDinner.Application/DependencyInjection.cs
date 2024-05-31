6210d288aede08f0f83372bd79d772e60eb1912b
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-05-31 21:20:44 +0300

    Refactoring cont

--- BurberDinner.Application/DependencyInjection.cs
+++ BurberDinner.Application/DependencyInjection.cs
@@ -1,4 +1,10 @@
 
+using System.Reflection;
+using BurberDinner.Application.Authentication.Commands.Register;
+using BurberDinner.Application.Authentication.Common;
+using BurberDinner.Application.Common.Behaviors;
+using ErrorOr;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,6 +15,10 @@ namespace BurberDinner.Application
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
       services.AddMediatR(typeof(DependencyInjection).Assembly);
+
+      services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+      services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
       return services;
     } 
